235953d85139e3b50f1826f44de4c0f076bc3173
hibian <416711267@qq.com>
2022-11-01 22:14:43 +0800

    A commit ？

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ import re
 
 var_aaa = 'var player_aaaa={"flag":"play","encrypt":0,"trysee":0,"points":0,"link":"\/bangumi\/8127\/play\/1\/1\/","link_next":"\/bangumi\/8127\/play\/2\/4\/","link_pre":"\/bangumi\/8127\/play\/2\/2\/","url":"d0a2ptjrMxeQzTQECBo-6niQbjk36ZN3bzpbbkDK6tg5uBYKDXr2i7it29rv2fefAW3Xh2_kdxFUmZq4n-wcYdDPf3-HAvTX2E3LdS0","url_next":"d0a2ptjrMxeQzTQECBo-6niQbjk36ZN3bzpbbkDK6tg5uBYKDXr2i7it29rv2fefAW3Xh2_kdxFUmZq4n-wcYdDPf3-HAvTX2E3LdS0","from":"danmakk","server":"no","note":"","id":"8127","sid":2,"nid":3}'
 print(ttt)
+# this is a test
